2eb93ecca2c29ab6fb281b9a39aa26188206a805
long1703@outlook.com <long1703@outlook.com>
2023-09-07 00:08:51 +0700

    update

--- src/hook/baseQueryWithReauth.ts
+++ src/hook/baseQueryWithReauth.ts
@@ -1,34 +1,43 @@
-import { BaseQueryFn, FetchArgs, FetchBaseQueryError, fetchBaseQuery } from '@reduxjs/toolkit/dist/query'
-import { RootState } from '../redux/store'
+import {
+  BaseQueryFn,
+  FetchArgs,
+  FetchBaseQueryError,
+  fetchBaseQuery,
+} from "@reduxjs/toolkit/dist/query";
+import { RootState } from "../redux/store";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl:"https://em-v2.oceantech.com.vn/em/",
-  credentials: 'include',
+  baseUrl: "https://em-v2.oceantech.com.vn/em/",
+  credentials: "include",
   prepareHeaders: (headers, { getState }) => {
-    const accessToken = getState() as RootState
-    console.log(accessToken)
+    const accessToken = (getState() as RootState).auth.user?.access_token;
+    // console.log(accessToken);
 
     if (accessToken) {
-      headers.set('authorization', `Bearer ${accessToken}`)
+      headers.set("Authorization", `Bearer ${accessToken}`);
     }
-    return headers
-  }
-})
+    return headers;
+  },
+});
 
-export const baseQueryWithReauth: BaseQueryFn<string | FetchArgs, unknown, FetchBaseQueryError> = async (
-  args,
-  api,
-  extraOptions
-) => {
-  const result = await baseQuery(args, api, extraOptions)
+export const baseQueryWithReauth: BaseQueryFn<
+  string | FetchArgs,
+  unknown,
+  FetchBaseQueryError
+> = async (args, api, extraOptions) => {
+  const result = await baseQuery(args, api, extraOptions);
   if (result.meta?.response?.status === 403) {
     // try to get a new token
-    const refreshToken = await baseQuery('/api/refreshToken', api, extraOptions) // Request refreshToken
+    const refreshToken = await baseQuery(
+      "/api/refreshToken",
+      api,
+      extraOptions
+    ); // Request refreshToken
     if (refreshToken.data) {
       // store the new token
-      const user = api.getState() as RootState // Cấp lại AccessToken
-      console.log(user, 'user accentoken')
+      const user = api.getState() as RootState; // Cấp lại AccessToken
+      console.log(user, "user accentoken");
     }
   }
-  return result
-}
+  return result;
+};
